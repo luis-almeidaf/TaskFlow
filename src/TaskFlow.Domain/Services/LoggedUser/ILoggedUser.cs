802e823b1f183c1f709154a1a2af3a28c76faa9f
Luis <luis.almeida.faria@gmail.com>
2025-11-03 19:59:51 -0300

    feat: implement funcionality to retrieve the logged user

---
+++ src/TaskFlow.Domain/Services/LoggedUser/ILoggedUser.cs
@@ -0,0 +1,8 @@
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Domain.Services.LoggedUser;
+
+public interface ILoggedUser
+{
+    Task<User> Get();
+}
---
+++ src/TaskFlow.Infrastructure/Services/LoggedUser/LoggedUser.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.Security.Tokens;
+using TaskFlow.Domain.Services.LoggedUser;
+using TaskFlow.Infrastructure.DataAccess;
+
+namespace TaskFlow.Infrastructure.Services.LoggedUser;
+
+public class LoggedUser : ILoggedUser
+{
+    private readonly TaskFlowDbContext _dbContext;
+    private readonly ITokenProvider _tokenProvider;
+
+    public LoggedUser(TaskFlowDbContext dbContext, ITokenProvider tokenProvider)
+    {
+        _dbContext = dbContext;
+        _tokenProvider = tokenProvider;
+    }
+
+    public async Task<User> Get()
+    {
+        var token = _tokenProvider.TokenOnRequest();
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+
+        var identifier = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value;
+
+        return await _dbContext.Users.AsNoTracking().FirstAsync(user => user.Id == Guid.Parse(identifier));
+    }
+}
