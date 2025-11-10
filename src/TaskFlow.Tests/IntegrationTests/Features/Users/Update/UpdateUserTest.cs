79834715e89968671a03a402b655bf9b191b05e7
Luis <luis.almeida.faria@gmail.com>
2025-11-10 10:19:48 -0300

    test: integration tests for update user feature

---
+++ src/TaskFlow.Tests/IntegrationTests/Features/Users/Update/UpdateUserTest.cs
@@ -0,0 +1,51 @@
+using System.Net;
+using System.Text.Json;
+using FluentAssertions;
+using TaskFlow.Exception;
+using TaskFlow.Tests.CommonTestUtilities.Commands;
+
+namespace TaskFlow.Tests.IntegrationTests.Features.Users.Update;
+
+public class UpdateUserTest : TaskFlowClassFixture
+{
+    private const string Route = "User";
+
+    private readonly string _token;
+
+    public UpdateUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var request = UpdateUserCommandBuilder.Build();
+
+        var response = await DoPut(Route, request, _token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Error_Empty_Name()
+    {
+        var request = UpdateUserCommandBuilder.Build();
+        request.Name = string.Empty;
+
+        var response = await DoPut(Route, request, _token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.NAME_EMPTY;
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
+
+    }
+}
