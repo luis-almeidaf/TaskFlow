b145e7dc5476660e7af54bf4c0da85997492ae50
Luis <luis.almeida.faria@gmail.com>
2025-11-12 19:14:02 -0300

    fix: change property Title to Name

--- src/TaskFlow.Application/Features/Boards/Commands/GetByID/Responses/ColumnResponse.cs
+++ src/TaskFlow.Application/Features/Boards/Commands/GetByID/Responses/ColumnResponse.cs
@@ -3,5 +3,5 @@ namespace TaskFlow.Application.Features.Boards.Commands.GetByID.Responses;
 public class ColumnResponse
 {
     public Guid Id { get; set; }
-    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 }
