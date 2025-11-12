203e140c2211880289ef4a37071db2ac08a9713a
Luis <luis.almeida.faria@gmail.com>
2025-11-12 19:13:09 -0300

    fix: remove DatetimeTime.UtcNow from CreatedAt property

--- src/TaskFlow.Application/Features/Boards/Commands/GetAll/Responses/ShortBoardResponse.cs
+++ src/TaskFlow.Application/Features/Boards/Commands/GetAll/Responses/ShortBoardResponse.cs
@@ -5,5 +5,5 @@ public class ShortBoardResponse
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public Guid CreatedById { get; set; }
-    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; }
 }
