d33559a8b917a0067593d4878875261d4f7bdfa0
Luis <luis.almeida.faria@gmail.com>
2025-11-27 18:52:47 -0300

    test: unit test for UpdateColumnCommandHandlerTest

--- src/TaskFlow.Tests/CommonTestUtilities/Entities/ColumnBuilder.cs
+++ src/TaskFlow.Tests/CommonTestUtilities/Entities/ColumnBuilder.cs
@@ -8,12 +8,12 @@ public class ColumnBuilder
     public static Column Build(Board board)
     {
         var columnNames = new List<string> { "backlog", "waiting" };
-        var columnPosition = new List<int> { 0, 1, 3, 4 };
+
 
         var column = new Faker<Column>()
             .RuleFor(column => column.Id, _ => Guid.NewGuid())
             .RuleFor(column => column.Name, faker => faker.PickRandom(columnNames))
-            .RuleFor(column => column.Position, faker => faker.PickRandom(columnPosition))
+            .RuleFor(column => column.Position, _ => 3)
             .RuleFor(column => column.BoardId, _ => board.Id);
         
         return column;
