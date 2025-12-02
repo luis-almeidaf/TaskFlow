b2cee76946a6b9214c702c76cf4c0309b5d84f0d
Luis <luis.almeida.faria@gmail.com>
2025-12-02 20:48:46 -0300

    chore: better spacing in Card class

--- src/TaskFlow.Domain/Entities/Card.cs
+++ src/TaskFlow.Domain/Entities/Card.cs
@@ -14,9 +14,11 @@ public class Card
     [InverseProperty("CreatedCards")]
     public Guid CreatedById { get; set; }
     public User CreatedBy { get; set; } = null!;
+    
     public Guid? AssignedToId { get; set; }
     [InverseProperty("AssignedCards")]
     public User? AssignedTo { get; set; }
+    
     public Guid ColumnId { get; set; }
     public Column Column { get; set; } = null!;
 }
